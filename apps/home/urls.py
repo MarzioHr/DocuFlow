b08f881adddbaa24d3be1f43023cd8c51e0fdb65
Marzio Hruschka <marzio.hruschka@tietoevry.com>
2023-02-09 15:25:17 +0100

    Added Backend logics and sidenav

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -4,9 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/new', views.create_doc, name='create-documentation'),
-    path('/list', views.documentations, name='list-documentation'),
-    path('/list/<str:bu>', views.filtered_documentations, name='filtered-documentation'),
+    path('new', views.create_doc, name='create-documentation'),
+    path('success', views.success, name='creation-success'),
+    path('list', views.documentations, name='list-documentation'),
+    path('list/<str:bu>', views.filtered_documentations, name='filtered-documentation'),
 
     # Common URLs
     re_path(r'^.*\.*', views.pages, name='pages'),
