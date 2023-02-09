6c950098da983bf7dd5ec8adf36c33e7de274623
Marzio Hruschka <marzio.hruschka@tietoevry.com>
2023-02-09 11:14:36 +0100

    Framework for Models and Views

--- apps/home/models.py
+++ apps/home/models.py
@@ -1,10 +1,31 @@
-# -*- encoding: utf-8 -*-
-"""
-Copyright (c) 2019 - present AppSeed.us
-"""
-
 from django.db import models
-from django.contrib.auth.models import User
+import uuid
+from django.contrib.auth.models import User, AbstractUser, Group, Permission
 
 # Create your models here.
+class User(AbstractUser):
+    '''
+    Add roles to the native Django User Model.
+    '''
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    groups = models.ManyToManyField(Group, null=True, blank=True, related_name='home_user_groups')
+    user_permissions = models.ManyToManyField(Permission, null=True, blank=True, related_name='home_user_permissions')
 
+class BusinessUnit(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=200, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    content = models.CharField(max_length=200, null=False, blank=False)
+
+class Documentation(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    heading = models.CharField(max_length=200, null=False, blank=False)
+    content = models.TextField(null=False, blank=False)
+    tags = models.ManyToManyField(Tag, null=True, blank=True, related_name='tags')
+    author = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE, related_name='author')
+    business_unit = models.ManyToManyField(BusinessUnit, null=True, blank=True, related_name='business_units')
--- apps/home/views.py
+++ apps/home/views.py
@@ -8,7 +8,10 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
+from django.shortcuts import render, redirect
+from .models import *
 
+TEMPLATE = 'home/index.html'
 
 @login_required(login_url="/login/")
 def index(request):
@@ -16,6 +19,26 @@ def index(request):
 
     html_template = loader.get_template('home/index.html')
     return HttpResponse(html_template.render(context, request))
+
+# Custom Pages
+def create_doc(request):
+     if request.method == 'POST':
+        try:
+            # OBJECT CREATION
+            pass
+        except Exception as e:
+            return redirect(f'/error')
+        else:
+            return redirect(f'/success')
+
+def documentations(request):
+    documentations = Documentation.objects.get.all()
+    return render(request, TEMPLATE, {"documentations":documentations})
+
+def filtered_documentations(request,bu):
+    business_unit = BusinessUnit.objects.filter(name=bu)
+    documentations = Documentation.objects.filter(business_unit=business_unit)
+    return render(request, TEMPLATE, {"documentations":documentations})
 
 
 @login_required(login_url="/login/")
